8f813921d24cee6ff16785de98cdc98185ca4bfd
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-06-04 00:10:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
     
-    window.location = `https://www.google.com/search?q=site%3Amenshealth.com/grooming+${randSearch}&sca_esv=44f1fb0a97cf7ff0&sca_upv=1&ei=a88bZqmBOJGE4-EPgYKxiA4&ved=0ahUKEwjp4oHO3cGFAxURwjgGHQFBDOEQ4dUDCBA&uact=5&oq=site%3Amenshealth.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXNpdGU6bWVuc2hlYWx0aC5jb20gYUiqjAhQAFgAcAR4AJABAJgBAKABAKoBALgBA8gBAJgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
+    window.location = `http://127.0.0.1:9117/UI/Dashboard#search=${randSearch}&filter=all`
   }
   return (
     <div className='App' onClick={handleClick}>
